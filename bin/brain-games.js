86b310ef748dc8688195b71c0f23f06474a1b2eb
REJO1CE <georgylitvinov2000@yandex.ru>
2024-01-19 16:03:58 +0300

    step3

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
 console.log("Hello from brain-games!");
+import { welcomeUser } from '../src/cli';
+
+welcomeUser();
---
+++ src/cli.js
@@ -0,0 +1,8 @@
+// src/cli.js
+import readlineSync from 'readline-sync';
+
+export const welcomeUser = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
